8bd07266690e3ef12e801da6399b9201118abc94
fsarrias <fsarrias@kolab.es>
2022-08-17 12:25:24 +0200

    stripe

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import { getProducts } from 'lib/data.js'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import * as localForage from 'localforage'
+import Script from 'next/script'
+
 
 
 export default function Home({products}) {
@@ -23,6 +25,8 @@ export default function Home({products}) {
 
   return (
     <div>
+            <Script src='https://js.stripe.com/v3/' />
+
       <Head>
         <title>Shop</title>
         <meta name='description' content='Shop' />
@@ -52,7 +56,39 @@ export default function Home({products}) {
                 </div>
               </div>
             ))}{' '}
+            
+            <button
+              className='mx-auto bg-black text-white px-3 py-1 my-4 text-xl font-bold justify-center flex'
+              onClick={async () => {
+                const res = await fetch('/api/stripe/session', {
+                  body: JSON.stringify({
+                    cart,
+                  }),
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  method: 'POST',
+                })
+                const data = await res.json()
 
+              if (data.status === 'error') {
+           alert(data.message)
+             return
+               }
+
+             const sessionId = data.sessionId
+             const stripePublicKey = data.stripePublicKey
+
+            const stripe = Stripe(stripePublicKey)
+            const { error } = await stripe.redirectToCheckout({
+             sessionId,
+             })
+
+              setCart([])
+              }}
+            >
+              Go to checkout
+            </button>
           <button
               className='mx-auto bg-black text-white px-3 py-1 my-4 text-sm justify-center flex'
               onClick={() => setCart([])}
